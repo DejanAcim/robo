76ac0c8c0531a19f881cdf83a2729285a6ea7d7f
dejanacim <dejanacim@gmail.com>
2017-05-07 18:03:36 +0200

    Setup navbar, sidebar and create product category index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-	get 'regulamin', to: "static#terms", as: :terms
-	get 'polityka-prywatnosci', to: "static#privacy", as: :privacy
-	get 'dostawa', to: "static#shipping", as: :shipping
-	get 'o-sklepie', to: "static#about", as: :about
+	get 'regulamin', 						to: "static#terms", 	 as: :terms
+	get 'polityka-prywatnosci', to: "static#privacy",  as: :privacy
+	get 'dostawa', 							to: "static#shipping", as: :shipping
+	get 'o-sklepie', 						to: "static#about", 	 as: :about
 
   root to: "products#index"
   resources :products, 	 only: [:show, :index]
