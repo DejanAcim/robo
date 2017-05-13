2994ce02c393c6fc1f71b62036ce6e3d8e29d24a
dejanacim <dejanacim@gmail.com>
2017-05-13 20:05:28 +0200

    Correrct admin controller name

--- test/controllers/admin/categories_controller_test.rb
+++ test/controllers/admin/categories_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
-class Admin::CatrgoriesControllerTest < ActionDispatch::IntegrationTest
+class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get admin_catrgories_index_url
+    get admin_categories_index_url
     assert_response :success
   end
 
   test "should get new" do
-    get admin_catrgories_new_url
+    get admin_categories_new_url
     assert_response :success
   end
 
   test "should get edit" do
-    get admin_catrgories_edit_url
+    get admin_categories_edit_url
     assert_response :success
   end
 
